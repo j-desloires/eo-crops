cd65c20f2871eef568f3b874cabdb642fdf16d27
johann.desloires <johann.desloires@inrae.fr>
2022-06-14 16:26:32 +0200

    git_ignore

--- .gitignore.py
+++ .gitignore.py
@@ -1,6 +1,37 @@
+# Byte-compiled / optimized / DLL files
 __pycache__/
-eocrops.egg-info/
+*.py[cod]
+*$py.class
+
+# Distribution / packaging
+.Python
+env/
 build/
+develop-eggs/
+dist/
+downloads/
+eggs/
+.eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+wheels/
+*.egg-info/
+.installed.cfg
 *.egg
-eocrops/__pycache__/
-eocrops/config/
+
+# IDE config
+.idea/
+.vscode/
+
+# Files that Sphinx generates
+/docs/source/examples
+/docs/source/markdowns
+/docs/source/eotasks.rst
+/docs/source/reference/*.rst
+
+# pytest
+.pytest_cache/
+features/.pytest_cache
