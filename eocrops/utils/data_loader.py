cc6634d68ba5fc0f6da6fbf175a79d07450e6fc7
johann.desloires <johann.desloires@inrae.fr>
2022-09-11 21:20:45 +0200

    update data_loader

--- eocrops/utils/data_loader.py
+++ eocrops/utils/data_loader.py
@@ -8,7 +8,6 @@ import os
 from eolearn.geometry import ErosionTask
 import eocrops.tasks.preprocessing as preprocessing
 import warnings
-from scipy import interpolate as interpolate
 from eocrops.tasks.vegetation_indices import VegetationIndicesS2 as s2_vis
 ###########################################################################################################
 
@@ -41,7 +40,7 @@ class EOPatchDataset:
         self.range_doy = range_doy
 
         if resampling is None:
-            resampling = dict(start = '-01-01', end = '-12-31', day_periods = 8)
+            resampling = dict(start = '01-01', end = '12-31', day_periods = 8)
             warnings.warn(
                 'You must specify a resampling periods to make your observations comparable. The default is set to ' + str(resampling))
         self.resampling = resampling
@@ -75,54 +74,63 @@ class EOPatchDataset:
         eopatch = interp.execute(eopatch)
         return eopatch
 
-    def _resamping_timeseries(self, arr, doy):
-        '''
-        Resample time series over a new periods after double logistic curve fitting
-        '''
-
-        xnew = np.arange(0, 365, self.resampling['day_periods'])
-        start = np.where(xnew >= doy[0])[0][0]
-        end = np.where(xnew <= doy[-1])[0][-1]
-
-        flinear_cspline = interpolate.Akima1DInterpolator(doy, arr)
-        ylinear_cspline = flinear_cspline(xnew[start:end])
-
-        before_season = np.repeat(ylinear_cspline[0], xnew[:start].shape[0])
-        before_season = before_season.reshape(before_season.shape[0] // ylinear_cspline.shape[1],
-                                              ylinear_cspline.shape[1])
-
-        after_season = np.repeat(ylinear_cspline[-1], xnew[end:].shape[0])
-        after_season = after_season.reshape(after_season.shape[0] // ylinear_cspline.shape[1],
-                                              ylinear_cspline.shape[1])
-
-        return np.concatenate([before_season, ylinear_cspline, after_season], axis=0)
 
     def _execute_gap_filling(self, eopatch,
                              resampled_range,
                              copy_features,
                              algorithm = 'linear'):
 
-        '''Gap filling after data extraction, very useful if did not include it in the data extraction workflow'''
+        """
+        Gap-filling to interpolate missing pixels and/or resample time series
+        Parameters
+        ----------
+        eopatch (EOPatch) : patch to preprocess
+        resampled_range (tuple) : resampled range (e.g. ("2020-01-01", "2020-12-12"))
+        copy_features (list) : original features to keep in the EOPatch
+        algorithm (str) : type of algorithm to do gap-filling
+
+        Returns EOPatch
+        -------
+
+        """
         kwargs = dict(copy_features=copy_features,
                       resampled_range=resampled_range,
                       algorithm = algorithm)
 
-        features = []
-        for ftype, fname, _, _, _ in self.features_data:
-            features.append((FeatureType.DATA, fname))
-        new_eopatch = self._interpolate_feature(eopatch=eopatch, features=features, **kwargs)
+        features = [
+            (FeatureType.DATA, fname)
+            for ftype, fname, _, _, _ in self.features_data
+        ]
 
-        return new_eopatch
+        return self._interpolate_feature(eopatch=eopatch, features=features, **kwargs)
 
-    def _prepare_eopatch(self, patch, resampled_range, algorithm = 'linear'):
+    def _prepare_eopatch(self, patch, resampled_range, algorithm = 'linear', disk_radius = 1):
+        '''
+        Preprocess EOPatch (making pixels, gap filling and temporal resampling)
+        Parameters
+        ----------
+        patch (EOPatch) : patch to preprocess
+        resampled_range (tuple) : period to resample the patch
+        algorithm (str) : typle of algorithm for gap filling (linear or cubic)
+        disk_radius (int) : Number of pixels to erode w.r.t field boundaries
+
+        Returns EOPatch
+        -------
+
+        '''
+        if algorithm not in ['linear', 'cubic']:
+            raise ValueError('Algorithm can only be "linear" of "cubic"')
 
         polygon_mask = (patch.data_timeless['FIELD_ID']>0).astype(np.int32)
         add_feature = AddFeatureTask(self.mask)
         add_feature.execute(eopatch=patch, data=polygon_mask.astype(bool))
         #patch.add_feature(self.mask[0], self.mask[1], polygon_mask.astype(bool))
 
-        erode = ErosionTask(mask_feature=self.mask, disk_radius=1)
+        erode = ErosionTask(mask_feature=self.mask, disk_radius=disk_radius)
         erode.execute(patch)
+
+        add_vis = s2_vis(biophysical=False, feature_name=self.bands_name)
+        add_vis.execute(eopatch=patch)
 
         patch = self._execute_gap_filling(eopatch=patch,
                                           resampled_range=resampled_range,
@@ -133,26 +141,39 @@ class EOPatchDataset:
     @staticmethod
     def _retrieve_range_doy(path, meta_file, range_doy, path_column,
                             planting_date_column, harvest_date_column):
+        '''
+        Get for each EOPatch the start and end of the season.
+
+        Parameters
+        ----------
+        path (str) : path of the EOPatch saved
+        meta_file (pd.DataFrame) : DataFrame with additional information for each EOPatch
+        range_doy (tuple) : approximate range of starting and ending of the season if not planting and harvest dates available
+        path_column (str) : column name from the meta_file with EOPatch paths
+        planting_date_column (str) : column name from the meta_file with EOPatch paths
+        harvest_date_column (str) : column name from the meta_file with EOPatch paths
+
+        Returns (tuple)  start of the season and the end of the season.
+        -------
+
+        '''
 
         meta_file_subset = meta_file[meta_file[path_column] == path]
         if meta_file_subset.shape[0] == 0:
             raise ValueError('We cannot find a correspond path for the patch in the meta file')
-        else:
-            if planting_date_column is not None:
-                if planting_date_column not in meta_file_subset.columns:
-                    raise ValueError('The column ' + planting_date_column + ' is not in the meta file')
-                else:
-                    range_doy[0] = meta_file_subset[planting_date_column].values[0]
-                    if np.isnan(range_doy[0]):
-                        range_doy[0] = np.nanmedian(meta_file[planting_date_column].values)
+        if planting_date_column is not None:
+            if planting_date_column not in meta_file_subset.columns:
+                raise ValueError('The column ' + planting_date_column + ' is not in the meta file')
+            range_doy[0] = meta_file_subset[planting_date_column].values[0]
+            if np.isnan(range_doy[0]):
+                range_doy[0] = np.nanmedian(meta_file[planting_date_column].values)
 
-            if harvest_date_column is not None:
-                if harvest_date_column not in meta_file_subset.columns:
-                    raise ValueError('The column ' + harvest_date_column + ' is not in the meta file')
-                else:
-                    range_doy[1] = meta_file_subset[harvest_date_column].values[0]
-                    if np.isnan(range_doy[1]):
-                        range_doy[1] = np.nanmedian(meta_file[harvest_date_column].values)
+        if harvest_date_column is not None:
+            if harvest_date_column not in meta_file_subset.columns:
+                raise ValueError('The column ' + harvest_date_column + ' is not in the meta file')
+            range_doy[1] = meta_file_subset[harvest_date_column].values[0]
+            if np.isnan(range_doy[1]):
+                range_doy[1] = np.nanmedian(meta_file[harvest_date_column].values)
 
         return range_doy
 
@@ -161,29 +182,47 @@ class EOPatchDataset:
                     doubly_logistic = False,
                     asym_gaussian = False,
                     return_params = False,
-                    fit_resampling = False,
                     meta_file = None,
                     path_column = None,
                     planting_date_column = None, harvest_date_column = None,
                     window_planting = 0, window_harvest = 0):
 
-        """ TF op for reading an eopatch at a given path. """
+        '''
+        Read and preprocess EOPatch given a path.
+        Asymmetric and logistic function can be fitted (https://www.sciencedirect.com/science/article/abs/pii/S0034425712001629)
+
+        Parameters
+        ----------
+        path (str) : path where the EOPatch is saved
+        algorithm (str) : type of algorithm to interpolate missing pixels
+        doubly_logistic (bool) : reconstruct time series using doubly logistic fitting
+        asym_gaussian (bool) : reconstruct time series using asymmetric gaussian
+        return_params (bool) : return parameters estimated from doubly or assymetric functions to get growth metrics
+        meta_file (pd.DataFrame) : DataFrame with meta info regarding planting and dates. Useful for thresholding before preprocessing.
+        path_column (str) : column from meta_file which give the EOPatch path for a given observation.
+        planting_date_column (str) : column from meta_file where planting date column (DOY) is referred
+        harvest_date_column (str) : column from meta_file where harvest date column (DOY) is referred
+        window_planting (int) : window before planting date to fit asymmetric ou doubly before N days
+        window_harvest (int) : window after planting date to fit asymmetric ou doubly after N days
+
+        Returns (np.arrray) : 3D np.array (1, t, d) ~ EOPatch resampled
+        -------
+
+        '''
         def _func(path):
             path = path.numpy().decode('utf-8')
             # Load only relevant features
             ################################################################
             #path = os.path.join(root_dir, os.listdir(root_dir)[0])
             patch = EOPatch.load(path)
-            vegetation_indices = s2_vis(feature_name=self.bands_name,
-                                        biophysical=False)
-            vegetation_indices.execute(patch)
 
-            if not doubly_logistic:
-                year = str(patch.timestamp[0].year)
-                start, end = year + self.resampling['start'], year + self.resampling['end']
-                resampled_range = (start, end, self.resampling['day_periods'])
-            else:
+            if doubly_logistic or asym_gaussian:
                 resampled_range = None
+            else:
+                year = str(patch.timestamp[0].year)
+                start, end = '{0}-{1}'.format(year, self.resampling['start']),\
+                             '{0}-{1}'.format(year, self.resampling['end'])
+                resampled_range = (start, end, self.resampling['day_periods'])
 
             patch = self._prepare_eopatch(patch, resampled_range, algorithm)
             range_doy = self.range_doy
@@ -192,23 +231,22 @@ class EOPatchDataset:
                 range_doy = self._retrieve_range_doy(path, meta_file, list(range_doy),
                                                      path_column, planting_date_column,
                                                      harvest_date_column)
-            if doubly_logistic:
-                curve_fitting = preprocessing.DoublyLogistic(range_doy=tuple([int(range_doy[0]) - window_planting,
-                                                                                  int(range_doy[1]) + window_harvest]))
 
-            elif asym_gaussian:
-                curve_fitting = preprocessing.AsymmetricGaussian(range_doy = tuple([int(range_doy[0]) - window_planting,
-                                                                                int(range_doy[1]) + window_harvest]))
+            range_doy = tuple([int(range_doy[0]) - window_planting,
+                               int(range_doy[1]) + window_harvest])
+
+            curve_fitting = preprocessing.DoublyLogistic(range_doy = range_doy)
+
+            if asym_gaussian:
+                curve_fitting = preprocessing.AsymmetricGaussian(range_doy = range_doy)
 
             #################################################################
             data = []
 
             for feat_type, feat_name, _, dtype, _ in self.features_data:
-                if doubly_logistic:
-                    if fit_resampling:
-                        resampling = self.resampling['day_periods']
-                    else:
-                        resampling = 0
+                if doubly_logistic or asym_gaussian:
+
+                    resampling = self.resampling['day_periods']
 
                     doy, arr = curve_fitting.execute(eopatch = patch,
                                                      feature=feat_name,
@@ -219,8 +257,6 @@ class EOPatchDataset:
                     if return_params:
                         data.append(params)
                     else:
-                        if not fit_resampling:
-                            arr = self._resamping_timeseries(arr.reshape(arr.shape[0], 1), doy)
                         data.append(arr)
                 else:
                     arr = curve_fitting.get_time_series_profile(eopatch=patch,
@@ -244,10 +280,19 @@ class EOPatchDataset:
         return out_data
 
     def _read_vector_data(self, path, column_path, vector_data, features_list):
-        """
-        TF op for reading an eopatch at a given path.
-        It must have a column with the corresponding path
-        """
+        '''
+        Subset the vector file to match each EOPatch
+
+        Parameters
+        ----------
+        vector_data (pd.DataFrame) : dataframe with meta info regarding each EOPatch saved
+        features_list (list) : list of features to read
+        column_path (str) : column name with the corresponding path of each EOPatch
+
+        Returns (np.array) : np.array from the input file
+        -------
+
+        '''
 
         def _func(path):
             path = path.numpy().decode('utf-8')
@@ -277,26 +322,42 @@ class EOPatchDataset:
         return np.concatenate(out_df, axis=0)
 
     def get_eopatch_tfds(self, algorithm = 'linear',
-                         doubly_logistic = False, return_params = False, fit_resampling = False,
+                         doubly_logistic = False,
+                         asym_gaussian = False,
+                         return_params = False,
                          meta_file=None,
                          path_column=None,
-                         planting_date_column=None, harvest_date_column=None,
+                         planting_date_column=None,
+                         harvest_date_column=None,
                          window_planting = 0, window_harvest = 0
                          ):
         '''
-        Aggregate all the EOPatch files into a single 3D array, where each observation is summarized muiltivariate time series (e.g. median NDVI and NDWI) of one EOPatch
 
         Parameters
         ----------
-        algorithm (str): name of the algorithm for gapfilling (linear or cubic)
+        algorithm (str) : type of algorithm to interpolate missing pixels
+        doubly_logistic (bool) : reconstruct time series using doubly logistic fitting
+        asym_gaussian (bool) : reconstruct time series using asymmetric gaussian
+        return_params (bool) : return parameters estimated from doubly or assymetric functions to get growth metrics
+        meta_file (pd.DataFrame) : DataFrame with meta info regarding planting and dates. Useful for thresholding before preprocessing.
+        path_column (str) : column from meta_file which give the EOPatch path for a given observation.
+        planting_date_column (str) : column from meta_file where planting date column (DOY) is referred
+        harvest_date_column (str) : column from meta_file where harvest date column (DOY) is referred
+        window_planting (int) : window before planting date to fit asymmetric ou doubly before N days
+        window_harvest (int) : window after planting date to fit asymmetric ou doubly after N days
+
+        Returns Returns (np.arrray) : 3D np.array (N, t, d) ~ EOPatch aggregated and resampled into the same np.array
+        -------
+
         '''
+
 
         self._instance_tf_ds()
         ds_numpy = self.dataset.map(lambda x : self._read_patch(path = x,
                                                                 algorithm=algorithm,
                                                                 doubly_logistic = doubly_logistic,
+                                                                asym_gaussian = asym_gaussian,
                                                                 return_params=return_params,
-                                                                fit_resampling = fit_resampling,
                                                                 meta_file=meta_file,
                                                                 path_column=path_column,
                                                                 planting_date_column=planting_date_column,
@@ -308,10 +369,19 @@ class EOPatchDataset:
         out_feature = list(ds_numpy)
         return self._format_feature(out_feature)
 
-
     def get_vector_tfds(self, vector_data, features_list, column_path):
         '''
-        Get ground truth data from a given dataframe into a 2D array. Each observation will match with the aggregation of EOPatch
+        Read the vector file into a numpy array.
+        Each observation will match the eopatch_tfds and will be converted into a np.array
+        Parameters
+        ----------
+        vector_data (pd.DataFrame) : dataframe with meta info regarding each EOPatch saved
+        features_list (list) : list of features to read
+        column_path (str) : column name with the corresponding path of each EOPatch
+
+        Returns (np.array) : np.array from the input file
+        -------
+
         '''
 
         self._instance_tf_ds()
@@ -324,6 +394,3 @@ class EOPatchDataset:
         npy_labels = npy_labels.reshape(npy_labels.shape[0], npy_labels.shape[-1])
 
         return npy_labels
-
-
-
---
+++ examples/dev_test/data_load.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from eolearn.core import EOPatch, FeatureType, EOTask
+import numpy as np
+import os
+from eocrops.utils import data_loader
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#################################################################################
+
+#Aggregate all the EOPatch saved into a root directory into a single 3D np.array, where each observation is an aggregated multiviariate time series
+#The objective is to get a dataset for machine learning project (e.g. yield prediction) where we work at the object level (e.g. averaged time series)
+
+#################################################################################
+#Features of EOPatch to load in the final dataset
+features_data = [
+    (FeatureType.DATA, 'LAI', 'LAI', 'float32', 1),
+    (FeatureType.DATA, 'fapar', 'fapar', 'float32', 1),
+    (FeatureType.DATA, 'Cab', 'Cab', 'float32', 1)]
+
+
+root_dir = '/home/johann/Documents/DATA/EOPatch samples/'
+files = os.listdir(root_dir)
+
+#Create an example of vector dataset with auxilliary data. It must have a column 'path' to match with the corresponding EOPatch
+meta_file = pd.DataFrame(
+    dict(
+        biomass = [np.random.normal(1) for i in files],
+        path = [os.path.join(root_dir, f) for f in files],
+        year = [2017 for i in files],
+        days_planting=[100 for i in files],
+        days_harvest=[300 for i in files],
+    )
+)
+
+feature_vector = [
+    ('biomass', 'float32'),
+    ('days_planting', 'int16'),
+    ('days_harvest', 'int16'),
+    ('path', 'string')
+]
+
+#################################################################################
+#Data loader pipeline
+pipeline_eopatch_tfds = data_loader.EOPatchDataset(
+    root_dir = root_dir, #root directory where EOPatch are saved
+    features_data=features_data, #features to read in EOPatch
+    suffix='_S2_L2A', #suffix for eopatch file names to filter out only one data source (e.g. S2_L2A). Put '' if you want to read all the files in root_dir
+    resampling = dict(start = '01-01', end = '12-31', day_periods = 8), #resample eopatch into 8 days period
+    range_doy=(100,300), #subset in term of doy to apply doubly logistic only on a subset period of the year
+    function=np.nanmedian #get average time series from the field
+)
+
+
+#Get the 3D array dataset with interpolation over the resampling period
+##Load data and resample time sereis using cubic intepolation
+npy_eopatch = pipeline_eopatch_tfds.get_eopatch_tfds(algorithm='cubic',
+                                                     meta_file=meta_file,
+                                                     path_column='path')
+
+
+with open(os.path.join('./predfin/yield_in_season/tests/dataset', 'X_S2.npy'), 'wb') as f:
+    np.save(f, npy_eopatch)
+
+
+##Load data and resample time series using doubly logistic intepolation
+npy_eopatch_doubly = pipeline_eopatch_tfds.get_eopatch_tfds(algorithm='cubic',
+                                                            doubly_logistic=True,
+                                                            meta_file=meta_file,
+                                                            path_column='path',
+                                                            planting_date_column='days_planting',
+                                                            harvest_date_column= 'days_harvest')
+
+##Load data and resample time series using asymmetric gaussian intepolation
+npy_eopatch_asym = pipeline_eopatch_tfds.get_eopatch_tfds(algorithm='cubic',
+                                                          asym_gaussian=True,
+                                                          meta_file=meta_file,
+                                                          path_column='path',
+                                                          planting_date_column='days_planting',
+                                                          harvest_date_column= 'days_harvest')
+
+
+plt.plot(npy_eopatch[0,:,0])
+plt.plot(npy_eopatch_doubly[0,:,0])
+plt.plot(npy_eopatch_asym[0,:,0])
+plt.legend(["original", "doubly", "asym"], loc ="lower right")
+plt.show()
