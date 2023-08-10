2a9bd4f27081015ce4f8dbd1ab6c0e1bcb4bc4df
johann.desloires <johann.desloires@inrae.fr>
2023-08-10 08:18:34 +0200

    update setup

--- setup.py
+++ setup.py
@@ -12,13 +12,52 @@ def parse_requirements(file):
     )
 
 
-setup(
+import setuptools
+import os
+
+
+def parse_requirements(file):
+    return sorted(
+        (
+            {
+                line.partition("#")[0].strip()
+                for line in open(os.path.join(os.path.dirname(__file__), file))
+            }
+            - set("")
+        )
+    )
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
     name="eocrops",
-    python_requires=">=3.7",
-    version="1.0.0",
-    description="Wrapper designed for crop monitoring using Earth Observation data.",
+    version="2.0.0",
+    packages=[
+        "eocrops",
+        "eocrops.inputs",
+        "eocrops.tasks.base_experiments",
+        "eocrops.utils.base_models",
+        "eocrops.climatools",
+    ],
+    url="https://github.com/j-desloires/eo-crops",
     author="Johann Desloires",
     author_email="johann.desloires@gmail.com",
-    packages=find_packages(),
-    package_data={"eocrops": ["environment.yml"]},
+    long_description=open("README.md", encoding="utf8").read(),
+    python_requires=">=3, <4",
+    extras_require={
+        "PROD": parse_requirements("requirements.txt"),
+        "DEV": parse_requirements("requirements-doc.txt"),
+    },
+    # Used for whole testing before pip finalized the install
+    test_suite="eocrops.tests.suite",
+    # Classifiers list used when deploying to PY.org
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: Not Distributable",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
 )
